2c8d61a0c1a21ccb2c6c826fbc2aab8876809f6d
Shay Hill <shay_public@hotmail.com>
2023-01-19 21:51:35 -0600

    feat: map Todoist dict ids to tree nodes

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # todoist_tree
+
+These are the core functions of [todoist_bot](https://github.com/ShayHill/todoist_bot).
+
+The reading and writing functions are just (sometimes typeguarded) aliases of Todoist api calls. The differentiating functionality is building a tree with
+
+``` python
+import time
+from todoist_tree import headers
+from todoist_tree import read_changes
+from todoist_tree import tree
+
+headers = new_headers(api_token)
+todoist = None
+sync_token: str = "*"
+
+complete = False
+
+while not complete:
+
+    todoist = read_changes.read_changes(headers)
+    if todoist is None:
+        # no changes or failure
+        time.sleep(2)
+        continue
+
+    sync_token = todoist.sync_token
+
+    projects = todoist.projects
+    sections = todoist.sections
+    tasks = todoist.tasks
+
+    id2node = tree.map_id_to_branch(
+        todoist.projects,
+        todoist.sections,
+        todoist.tasks
+    )
+
+    # do something here
+
+    time.sleep(5)
+```
+
+The tree isn't doesn't have one root. `map_id_to_branch` maps the id[1] of each project, section, and task to a node. Top-level projects will not have parents, so they are effectively roots of their own trees.
+
+See [todoist_bot](https://github.com/ShayHill/todoist_bot) for a full example.
+
+[1] where `id` is the value returned in the json dictionary from the Todoist api, *not* the Python object id.
---
+++ src/todoist_tree/task_subsets.py
@@ -0,0 +1,127 @@
+"""Selection schemes for identifying tasks at or underneath an automation suffix.
+
+:author: Shay Hill
+:created: 2022-12-12
+"""
+
+from contextlib import suppress
+from typing import Iterator, TypeAlias
+
+from todoist_tree.read_changes import Project, Section, Task
+from todoist_tree.tree import AnyNode
+
+_PST: TypeAlias = Project | Section | Task
+
+
+def _has_suffix(suffix: str, model: Project | Section | Task) -> bool:
+    """Select an automation from an element name.
+
+    :param elem: a Todoist Project, Section, or Task
+    :return: an Automation or None
+
+    Projects, Sections, and Tasks don't use the same attribute for the name. Tasks
+    use "content", Projects and Sections use "name".
+    """
+    if isinstance(model, (Project, Section)):
+        name = model.name
+    else:
+        name = model.content
+
+    return name.strip().endswith(suffix)
+
+
+def _filter_for_suffix(suffix: str, *model: _PST) -> Iterator[_PST]:
+    """Iterate over all elements that have a suffix.
+
+    :param projects: a list of Todoist Projects
+    :param sections: a list of Todoist Sections
+    :param tasks: a list of Todoist Tasks
+    :param suffix: the suffix to match
+    :return: an iterator over all elements that have a suffix
+
+    It takes some noise to keep mypy happy, so I'm wrapping the generator in a
+    function.
+    """
+    return (m for m in model if _has_suffix(suffix, m))
+
+
+def select_serial(
+    projects: list[Project],
+    sections: list[Section],
+    tasks: list[Task],
+    id2node: dict[str, AnyNode],
+    suffix: str,
+) -> tuple[list[Task], list[Task]]:
+    """Select next childless task at or under each marked model.
+
+    :param projects: a list of Todoist Projects
+    :param sections: a list of Todoist Sections
+    :param tasks: a list of Todoist Tasks
+    :param id2node: a mapping from Todoist model IDs to Nodes
+    :param suffix: a suffix to identify serial tasks
+    :return: a tuple of (selected, rejected) tasks
+    """
+
+    selected: dict[str, Task] = {}
+
+    for model in _filter_for_suffix(suffix, *projects, *sections, *tasks):
+        with suppress(StopIteration):
+            model_id = model.id
+            assert model_id is not None
+            with suppress(StopIteration):
+                next_task = next(id2node[model_id].iter_childless_tasks())
+                selected[next_task.id] = next_task
+
+    return list(selected.values()), [x for x in tasks if x.id not in selected]
+
+
+def select_parallel(
+    projects: list[Project],
+    sections: list[Section],
+    tasks: list[Task],
+    id2node: dict[str, AnyNode],
+    suffix: str,
+) -> tuple[list[Task], list[Task]]:
+    """Select every childless task at or under each marked model.
+
+    :param projects: a list of Todoist Projects
+    :param sections: a list of Todoist Sections
+    :param tasks: a list of Todoist Tasks
+    :param id2node: a mapping from Todoist model IDs to Nodes
+    :param suffix: a suffix to identify parallel tasks
+    :return: a tuple of (selected, rejected) tasks
+    """
+    selected: dict[str, Task] = {}
+
+    for model in _filter_for_suffix(suffix, *projects, *sections, *tasks):
+        model_id = model.id
+        assert model_id is not None
+        selected.update({x.id: x for x in id2node[model_id].iter_childless_tasks()})
+
+    return list(selected.values()), [x for x in tasks if x.id not in selected]
+
+
+def select_all(
+    projects: list[Project],
+    sections: list[Section],
+    tasks: list[Task],
+    id2node: dict[str, AnyNode],
+    suffix: str,
+) -> tuple[list[Task], list[Task]]:
+    """Select every task at or under each marked model.
+
+    :param projects: a list of Todoist Projects
+    :param sections: a list of Todoist Sections
+    :param tasks: a list of Todoist Tasks
+    :param id2node: a mapping from Todoist model IDs to Nodes
+    :param suffix: a suffix to identify parallel tasks
+    :return: a tuple of (selected, rejected) tasks
+    """
+    selected: dict[str, Task] = {}
+
+    for model in _filter_for_suffix(suffix, *projects, *sections, *tasks):
+        model_id = model.id
+        assert model_id is not None
+        selected.update({x.id: x for x in id2node[model_id].iter_tasks()})
+
+    return list(selected.values()), [x for x in tasks if x.id not in selected]
